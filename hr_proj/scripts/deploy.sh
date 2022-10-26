55a7647537878d1f893ba9fe5e6c60bf64f39412
SONGJAEHEON <spes2you@gmail.com>
2022-10-26 16:43:19 +0900

    update .sh

--- hr_proj/scripts/deploy.sh
+++ hr_proj/scripts/deploy.sh
@@ -1,51 +1,33 @@
 #!/bin/bash
 
-REPOSITORY=/home/ec2-user/app/step1/HYU_HR_Management
+REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=hr_proj
 
-cd $REPOSITORY/$PROJECT_NAME/
-
-echo "> Git Pull"
-
-git pull
-
-echo "> 프로젝트 Build 시작"
-
-./gradlew build
-
-echo "> steop1 디렉토리로 이동"
-
-cd $REPOSITORY
-
 echo "> Build 파일 복사"
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
+CURRENT_PID=$(pgrep -f $PROJECT_NAME)
 
-echo "> 현재 구동중인 애플리케이션 pid 확인"
-
-CURRENT_PID=${pgrep -f ${PROJECT_NAME}.*.jar}
-
-echo "현재 구동 중인 애플리케이션 pid: $:CURRENT_PID"
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-        echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다"
 else
-        echo "> kill -15 $CURRENT_PID"
-        kill -15 $CURRENT_PID
-        sleep 5
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
 fi
 
 echo "> 새 애플리케이션 배포"
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
-
-echo "> JAR Name: $JAR_NAME"
-echo "> $JAR_NAME에 실행 권한 추가"
+echo "> JAR_NAME: $JAR_NAME"
+echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
-
 nohup java -jar \
-        -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
-        -Dspring.profile.active=real \
-        $REPOSITORY/$JAR_NAME 2>&1 &
+  -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+  -Dspring.profile.active=real \
+  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
